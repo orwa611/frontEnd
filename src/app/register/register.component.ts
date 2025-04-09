7840b3f276029a1f2238abb802c4c8d835284395
ouroua kaddouri <ouroua.kaddouri@gmail.com>
2025-04-09 18:02:00 +0100

    adding form validation to registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,29 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Author } from '../interfaces/author';
-import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, NgIf, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup 
+  
   ngOnInit(): void {
-   
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      about: new FormControl(''),
+    });
+
   }
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  author: Author = {
-    name: "",
-    lastname: "",
-    email: "",
-    password: "",
-    about: undefined,
-  };
+  get name() {
+    return this.registerForm.get('name')!;
+  }
+
+  get lastname() {
+    return this.registerForm.get('lastname')!;
+  }
+
+  get email() {
+    return this.registerForm.get('email')!;
+  }
+  
+  get password() {
+    return this.registerForm.get('password')!;
+  }
+
+  get about() {
+    return this.registerForm.get('about')!;
+  }
  
   image : any;
 
@@ -32,17 +54,18 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    let fd = new FormData()
-    fd.append('image',this.image)
-    fd.append('name', this.author.name)
-    fd.append('lastname', this.author.lastname)
-    fd.append('email', this.author.email)
-    fd.append('password', this.author.password)
-    fd.append('about', this.author.about ?? "")
-    this.authService.register(fd).subscribe((value) =>{
-      this.router.navigate(['/login']);
-    })
-  
+    if(this.registerForm.valid) {
+      let fd = new FormData()
+      fd.append('image',this.image)
+      fd.append('name', this.name.value)
+      fd.append('lastname', this.lastname.value)
+      fd.append('email', this.email.value)
+      fd.append('password', this.password.value)
+      fd.append('about', this.about.value)
+      this.authService.register(fd).subscribe((_) =>{
+        this.router.navigate(['/login']);
+      })
+    }
   }
 
 }
