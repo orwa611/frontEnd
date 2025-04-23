a58df43220081cf34cc7fef5449b8620d05471f0
ouroua kaddouri <ouroua.kaddouri@gmail.com>
2025-04-23 19:14:11 +0100

    addind action buttons for edit and delete.

--- src/app/home/blog-list/blog-list.component.ts
+++ src/app/home/blog-list/blog-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Article } from '../../interfaces/article';
-import { RouterLink } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 import { getImageUrl } from '../../../constants/constants';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [RouterLink, NgFor],
+  imports: [RouterLink, NgFor, NgIf],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.css'
 })
 export class BlogListComponent {
+  private activatedRoute = inject(ActivatedRoute)
   @Input() articles: Article[] = [];
   
   getImage(image: string): string {
     return getImageUrl(image);
+  }
+
+  get isActionBtnEnabled(): boolean {
+    console.log(this.activatedRoute.routeConfig?.path?.includes('myAccount') == true)
+    return this.activatedRoute.routeConfig?.path?.includes('myAccount') == true
   }
 }
